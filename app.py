7ec345aaebd2297e4a8e3bc61eb198eed28ffd13
Eduardo Gomes <edunb01@hotmail.com>
2022-11-19 13:35:09 -0300

    primeira linha

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ client = storage.Client(credentials=credentials)
 @st.experimental_memo(ttl=600)
 def read_file(bucket_name, file_path):
     bucket = client.bucket(bucket_name)
-    content = bucket.blob(file_path).get_blob_to_text()
+    content = bucket.blob(file_path).download_blob_to_stream()
     return content
 
 bucket_name = "app_recomenda"
